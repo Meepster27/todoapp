75308ba38c1c8afa47e42a560d07383a1de07515
Meepster27 <sharpieisme27@gmail.com>
2026-04-02 00:45:31 +0000

    web

--- App.js
+++ App.js
@@ -121,7 +121,7 @@ const styles = StyleSheet.create({
   inputContainer: {
     paddingHorizontal: 20,
     paddingVertical: Platform.OS === 'ios' ? 40 : 30,
-    paddingBottom: Platform.OS === 'ios' ? 80 : 60,
+    paddingBottom: Platform.OS === 'ios' ? 80 : Platform.OS === 'android' ? 60 : 20,
     backgroundColor: '#e8f4f8',
     borderTopWidth: 1,
     borderTopColor: '#ddd',
